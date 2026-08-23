b714bca2ab3b0090420573a8d6a8be2ebb26b953
Jo Seraph <josephseraph@usp.br>
2026-08-23 01:27:52 -0300

    refactor: classify scene manager as presentation lifecycle

---
+++ src/presentation/scene_manager.js
@@ -0,0 +1,44 @@
+/**
+ * Presentation scene stack and browser-frame lifecycle.
+ *
+ * This is deliberately not engine state: it owns presentation Scene instances
+ * and the requestAnimationFrame loop that drives them.
+ */
+export class SceneManager {
+  constructor(container) {
+    this.container = container;
+    /** @type {import("./scenes/scenes.js").Scene_Base[]} */
+    this._stack = [];
+    /** @type {import("./scenes/scenes.js").Scene_Base|null} */
+    this._currentScene = null;
+    this.requestUpdate();
+  }
+
+  update() {
+    if (this._currentScene) this._currentScene.update();
+    this.requestUpdate();
+  }
+
+  requestUpdate() {
+    requestAnimationFrame(this.update.bind(this));
+  }
+
+  push(scene) {
+    if (this._currentScene) this._stack.push(this._currentScene);
+    this._currentScene = scene;
+    scene.start();
+  }
+
+  pop() {
+    if (this._currentScene) this._currentScene.stop();
+    this._currentScene = this._stack.pop();
+  }
+
+  currentScene() {
+    return this._currentScene;
+  }
+
+  previous() {
+    return this._stack[this._stack.length - 1];
+  }
+}
