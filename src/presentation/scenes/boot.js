f1f9df2f2a60bc6607ed4c7aebe8c1c7b934e1a6
Jo Seraph <josephseraph@usp.br>
2026-08-23 01:32:22 -0300

    refactor: name boot dependency as content loader

--- src/presentation/scenes/boot.js
+++ src/presentation/scenes/boot.js
@@ -7,54 +7,36 @@ import { SessionSerializer } from "../../engine/session/serializer.js";
 import { Game_Party } from "../../objects/party.js";
 import { QuestLogState } from "../../engine/session/quest_state.js";
 
-/**
- * Initial composition scene: acquires content, initializes content-dependent
- * presentation services, restores session state, then enters the map.
- */
+/** Initial composition scene. */
 export class Scene_Boot extends Scene_Base {
-    constructor(dataManager, sceneManager, windowManager) {
-        super(dataManager, windowManager);
+    constructor(contentLoader, sceneManager, windowManager) {
+        super(contentLoader, windowManager);
+        this.contentLoader = contentLoader;
         this.sceneManager = sceneManager;
     }
 
     async start() {
-        await this.dataManager.loadData();
-
-        Registry.set('items', this.dataManager.items);
-        Registry.set('skills', this.dataManager.skills);
-        Registry.set('actors', this.dataManager.actors);
-        if (this.dataManager.states) Registry.set('states', this.dataManager.states);
-        if (this.dataManager.enemies) Registry.set('enemies', this.dataManager.enemies);
-        if (this.dataManager.quests) Registry.set('quests', this.dataManager.quests);
-
-        ThemeManager.init(this.dataManager.themes);
-        if (this.dataManager.sounds) {
-            await AudioAdapter.initialize(this.dataManager.sounds);
-        }
+        await this.contentLoader.loadData();
+        Registry.set('items', this.contentLoader.items);
+        Registry.set('skills', this.contentLoader.skills);
+        Registry.set('actors', this.contentLoader.actors);
+        if (this.contentLoader.states) Registry.set('states', this.contentLoader.states);
+        if (this.contentLoader.enemies) Registry.set('enemies', this.contentLoader.enemies);
+        if (this.contentLoader.quests) Registry.set('quests', this.contentLoader.quests);
+        ThemeManager.init(this.contentLoader.themes);
+        if (this.contentLoader.sounds) await AudioAdapter.initialize(this.contentLoader.sounds);
 
         let session;
         const savedData = localStorage.getItem('stillnight_save_data');
         if (savedData) {
-            try {
-                session = SessionSerializer.fromJSON(JSON.parse(savedData));
-            } catch (e) {
-                console.error("Failed to load save:", e);
-                session = this._createNewSession();
-            }
-        } else {
-            session = this._createNewSession();
-        }
+            try { session = SessionSerializer.fromJSON(JSON.parse(savedData)); }
+            catch (e) { console.error("Failed to load save:", e); session = this._createNewSession(); }
+        } else session = this._createNewSession();
 
-        this.sceneManager.push(new Scene_Map(this.dataManager, this.sceneManager, this.windowManager, session));
+        this.sceneManager.push(new Scene_Map(this.contentLoader, this.sceneManager, this.windowManager, session));
     }
 
     _createNewSession() {
-        return {
-            party: new Game_Party(),
-            exploration: null,
-            battle: null,
-            interpreter: null,
-            quests: new QuestLogState()
-        };
+        return { party: new Game_Party(), exploration: null, battle: null, interpreter: null, quests: new QuestLogState() };
     }
 }
