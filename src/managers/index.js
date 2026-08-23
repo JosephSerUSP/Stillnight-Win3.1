44a76e33f6027f450f346e43efef6c69b2c0ceee
Jo Seraph <josephseraph@usp.br>
2026-08-23 01:30:44 -0300

    refactor: retire obsolete managers namespace barrel

--- src/managers/index.js
+++
@@ -1,5 +0,0 @@
-// Compatibility barrel for remaining infrastructure-era names.
-export { DataManager } from "./data.js";
-export { SoundManager } from "./sound.js";
-export { ConfigManager } from "./config.js";
-export { MidiParser, MidiPlayer } from "./midi.js";
