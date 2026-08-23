f127077a95da4fb84a0b5e208acd267642c3c833
Jo Seraph <josephseraph@usp.br>
2026-08-23 01:32:54 -0300

    refactor: name scene content dependency accurately

--- src/presentation/scenes/base.js
+++ src/presentation/scenes/base.js
@@ -1,11 +1,14 @@
 /** Abstract base class for presentation scenes. */
 export class Scene_Base {
   /**
-   * @param {import("../../data/content_loader.js").ContentLoader} dataManager static content loader
+   * @param {import("../../data/content_loader.js").ContentLoader} contentLoader static content loader
    * @param {import("../windows/index.js").WindowManager} windowManager presentation window manager
    */
-  constructor(dataManager, windowManager) {
-    this.dataManager = dataManager;
+  constructor(contentLoader, windowManager) {
+    // dataManager remains a compatibility property for existing scene code while
+    // its concrete owner is now accurately named and located as ContentLoader.
+    this.contentLoader = contentLoader;
+    this.dataManager = contentLoader;
     this.windowManager = windowManager;
   }
 
