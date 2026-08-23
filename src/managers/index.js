1ba67c376e0962e3269a7703ba6dcbe5f99c7f08
Jo Seraph <josephseraph@usp.br>
2026-08-23 01:28:04 -0300

    refactor: remove presentation lifecycle from managers barrel

--- src/managers/index.js
+++ src/managers/index.js
@@ -1,6 +1,5 @@
-// Re-export the remaining infrastructure managers from their individual files.
+// Compatibility barrel for remaining infrastructure-era names.
 export { DataManager } from "./data.js";
 export { SoundManager } from "./sound.js";
-export { SceneManager } from "./scene.js";
 export { ConfigManager } from "./config.js";
 export { MidiParser, MidiPlayer } from "./midi.js";
