1b1fefe7ca7e84507c9da4cb32eb590f2ce27fd7
Jo Seraph <josephseraph@usp.br>
2026-08-23 01:36:20 -0300

    refactor: preserve scene lifecycle implementation while relocating

--- src/presentation/scene_manager.js
+++ src/presentation/scene_manager.js
@@ -9,10 +9,38 @@ export class SceneManager {
     this._currentScene = null;
     this.requestUpdate();
   }
-  update() { if (this._currentScene) this._currentScene.update(); this.requestUpdate(); }
-  requestUpdate() { requestAnimationFrame(this.update.bind(this)); }
-  push(scene) { if (this._currentScene) this._stack.push(this._currentScene); this._currentScene = scene; scene.start(); }
-  pop() { if (this._currentScene) this._currentScene.stop(); this._currentScene = this._stack.pop(); }
-  currentScene() { return this._currentScene; }
-  previous() { return this._stack[this._stack.length - 1]; }
+
+  update() {
+    if (this._currentScene) {
+      this._currentScene.update();
+    }
+    this.requestUpdate();
+  }
+
+  requestUpdate() {
+    requestAnimationFrame(this.update.bind(this));
+  }
+
+  push(scene) {
+    if (this._currentScene) {
+      this._stack.push(this._currentScene);
+    }
+    this._currentScene = scene;
+    scene.start();
+  }
+
+  pop() {
+    if (this._currentScene) {
+      this._currentScene.stop();
+    }
+    this._currentScene = this._stack.pop();
+  }
+
+  currentScene() {
+    return this._currentScene;
+  }
+
+  previous() {
+    return this._stack[this._stack.length - 1];
+  }
 }
